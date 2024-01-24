d4f499320ec62708bf3763c8267af91b03d0999c
francois <totofrancois12@gmail.com>
2024-01-24 21:54:36 +0100

    add connection to database

---
+++ src/main/java/BDD/Database.java
@@ -0,0 +1,26 @@
+package BDD;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private String url = "jdbc:mysql://localhost/projet_lprs_java?serverTimezone=UTC";
+    private String user = "projetlprs";
+    private String password = "lprs18Po220@";
+    public Connection getConnection() {
+
+        try {
+            Connection cnx = DriverManager.getConnection(this.url,this.user,this.password);
+            //System.out.print("Etat de la connexion :");
+            //System.out.print(cnx.isClosed()?"fermée":"ouverte \r\n");
+            return cnx;
+
+        } catch (SQLException e) {
+            System.out.print("erreur");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+
